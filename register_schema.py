8a7d104fa6ffdec4ab72d498b5f82303798452bb
ratthaslip <89380151+ratthaslip@users.noreply.github.com>
2024-06-22 19:52:11 +0700

    Update register_schema.py

--- register_schema.py
+++ register_schema.py
@@ -8,9 +8,6 @@ schema_registry_url = 'http://13.212.25.xxx:8081'
 kafka_topic = 'ais-lab'
 schema_registry_subject = f"{kafka_topic}-value"
 
-username=" "
-password=" "
-schema_registry_url = f"https://{username}:{password}@ "
 
 def register_schema(schema_registry_url, schema_registry_subject, schema_str):
     sr = SchemaRegistryClient({'url': schema_registry_url})
